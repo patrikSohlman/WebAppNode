878d7ccd2d7ab4726b9e156a702d4571aa753a79
Patrik Sohlman <Patrik Sohlman@DESKTOP-DDE1HB6>
2018-01-30 14:57:35 +0100

    New ports settings

--- WebAppNode/server.js
+++ WebAppNode/server.js
@@ -2,6 +2,9 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
 var express = require('express');
+var WebSocket = require('ws');
+var moment = require('moment');
+var iotHubClient = require('./IoThub/iot-hub.js');
 var app = express();
 
 var controllers = require("./Controllers");
@@ -15,4 +18,55 @@ app.use(express.static(__dirname + "/public"));
 
 
 var server = http.createServer(app);
-server.listen(port);
+
+var wss = new WebSocket.Server({ server });
+
+// Broadcast to all.
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            try {
+                console.log('sending data ' + data);
+                client.send(data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    });
+};
+
+var iotHubReader = new iotHubClient(process.env['Azure.IoT.IoTHub.ConnectionString'], process.env['Azure.IoT.IoTHub.ConsumerGroup']);
+iotHubReader.startReadMessage(function (obj, date) {
+    try {
+        console.log(date);
+        date = date || Date.now()
+        wss.broadcast(JSON.stringify(Object.assign(obj, { time: moment.utc(date).format('YYYY:MM:DD[T]hh:mm:ss') })));
+    } catch (err) {
+        console.log(obj);
+        console.error(err);
+    }
+});
+var port = normalizePort(process.env.PORT || '3000');
+server.listen(port, function listening() {
+    console.log('Listening on %d', server.address().port);
+});
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
